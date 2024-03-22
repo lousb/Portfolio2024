47df12e48fd14686a64cdad6f3cf27dde0ef822a
lousb <wyethtafe@gmail.com>
2024-03-22 23:05:56 +1100

    mp4 additions

--- src/utils/delayLink.js
+++ src/utils/delayLink.js
@@ -4,51 +4,46 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 
 // Functional link component which delays page navigation
 export const DelayLink = props => {
-  const { delay, onDelayStart, onDelayEnd, replace, to, ...rest } = props;
-  let timeout = null;
-  let navigate = useNavigate();
-  let location = useLocation();
+ const { delay, onDelayStart, onDelayEnd, replace, to, ...rest } = props;
+ let timeout = null;
+ let navigate = useNavigate();
+ let location = useLocation();
 
-  useEffect(() => {
-    return () => {
-      if (timeout) {
-        clearTimeout(timeout);
-      }
-    };
-  }, [timeout]);
+ useEffect(() => {
+   return () => {
+     if (timeout) {
+       clearTimeout(timeout);
+     }
+   };
+ }, [timeout]);
 
-  const handleClick = e => {
-    // if trying to navigate to current page stop everything
-    if (location?.pathname === to) return;
+ const handleClick = e => {
+   // if trying to navigate to current page stop everything
+   if (location?.pathname === to) return;
 
-    // Check if screen width is less than 800px
-    if (window.innerWidth < 800) {
-      if (replace) {
-        navigate(to, { replace: true });
-      } else {
-        navigate(to);
-      }
-      return;
-    }
+   // Check if screen width is less than 800px
+   if (window.innerWidth < 800) {
+     return; // Remove redundant call to navigate(to) here
+   }
 
-    onDelayStart(e, to);
-    if (e.defaultPrevented) {
-      return;
-    }
+   onDelayStart(e, to);
+   if (e.defaultPrevented) {
+     return;
+   }
 
-    e.preventDefault();
+   e.preventDefault();
 
-    timeout = setTimeout(() => {
-      if (replace) {
-        navigate(to, { replace: true });
-      } else {
-        navigate(to);
-      }
-      onDelayEnd(e, to);
-    }, delay);
-  };
+   timeout = setTimeout(() => {
+     if (replace) {
+       navigate(to, { replace: true });
+     } else {
+       navigate(to);
+     }
+     onDelayEnd(e, to);
+   }, delay);
+ };
 
-  return <Link {...rest} to={to} onClick={handleClick} />;
+ return <Link {...rest} to={to} onClick={handleClick} />;
 };
 
 DelayLink.propTypes = {
